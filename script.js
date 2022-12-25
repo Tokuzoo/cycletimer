4ff73c5b227fca3931ff114ae2d9a2ce384a283a
Tokuzoo <74489240+Tokuzoo@users.noreply.github.com>
2022-12-25 17:44:04 +0900

    タスクの追加

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
+const $add = document.getElementById("add");
+const $task_container = document.getElementById("task_container");
+const $task_page = document.getElementById("task_page");
+const $timer_page = document.getElementById("timer_page");
 const $remain = document.getElementById("remain");
 const $toggleButton = document.getElementById("toggleButton");
 const $resetButton = document.getElementById("resetButton");
+const $confirm = document.getElementById("confirm");
+const $tasks_disp = document.getElementById("tasks_disp");
+
+$add.onclick = function() {
+    let div = document.createElement("div");
+    div.className = "tasks_div";
+    $task_container.appendChild(div);
+
+    let form = document.createElement("input");
+    form.className = "tasks_form";
+
+    let del = document.createElement("button");
+    del.className = "tasks_del";
+
+    div.appendChild(form);
+    div.appendChild(del);
+
+    $tasks_del = document.getElementsByClassName("tasks_del");
+    del_handler($tasks_del);
+}
+
+$confirm.onclick = function() {
+    $task_page.classList.add("hidden");
+    $timer_page.classList.toggle("hidden");
+
+    for(let i=0; i<$tasks_del.length; i++){
+        let div = document.createElement("div");
+        div.className = "disp_div";
+        $tasks_disp.appendChild(div);
+
+        let task = document.createElement("p");
+        task.textContent = document.getElementsByClassName("tasks_form")[i].value;
+        task.className = "disp_task";
+        div.appendChild(task);
+
+        let fin = document.createElement("button");
+        fin.className = "disp_fin";
+        div.appendChild(fin);
+
+        $disp_fin = document.getElementsByClassName("disp_fin");
+        fin_handler($disp_fin);
+    }
+}
 
 let time = -1;
 let setTime = -1;
@@ -81,4 +128,26 @@ function update() {
 function getValues() {
     window.$studySet = document.getElementById("studySet");
     window.$breakSet = document.getElementById("breakSet");
+}
+
+let k;
+
+const del_handler = (c) => {
+    c[c.length-1].addEventListener('click', (e) => {
+        k = Array.prototype.indexOf.call(c, e.target);
+    
+        $div = document.getElementsByClassName("tasks_div");
+
+        $div[k].remove();
+    })
+}
+
+const fin_handler = (c) => {
+    c[c.length-1].addEventListener('click', (e) => {
+        k = Array.prototype.indexOf.call(c, e.target);
+
+        $div = document.getElementsByClassName("disp_div");
+
+        $div[k].remove();
+    })
 }
